3c724bd039e3d907d81731bb9da8abe6bdce85bd
Julian Paquerot <111422357+Kajiih@users.noreply.github.com>
2024-11-10 17:24:45 +0100

    🔊 Add post hook message

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,4 +6,6 @@ import pathlib
 if __name__ == "__main__":
     if "Not open source" == "{{ cookiecutter.open_source_license }}":  # noqa: PLR0133
         pathlib.Path("LICENSE").unlink()
+
 print("GitHub repo name: {{ cookiecutter.project_slug }}")
+print("To setup the project, run: bash {{ cookiecutter.project_slug }}/setup_project.sh")
